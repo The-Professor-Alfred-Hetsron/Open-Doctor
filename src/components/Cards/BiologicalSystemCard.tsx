1add7d8077aae4e189300f4b22f89a63c704246a
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2024-01-23 15:56:22 +0100

    Added System Images

--- src/components/Cards/BiologicalSystemCard.tsx
+++ src/components/Cards/BiologicalSystemCard.tsx
@@ -19,7 +19,7 @@ const BiologicalSystemCard:React.FC<PropsTypes> = ({
         href={cases>0?{pathname: "/tutoring", query: { system: name}}:"/"}
         className='w-[300px] h-[400px] p-4 flex flex-col rounded-lg bg-white border-2 border-primary-blue shadow-md drop-shadow-lg hover:scale-[110%] hover:bg-secondary-yellow hover:text-white hover:border-white'
     >
-        <Image src={`${image}`} alt={name} width={200} height={200} className='w-full h-1/2'/>
+        <Image src={`${image}`} alt={name} width={200} height={200} className='w-full h-1/2 rounded-lg'/>
         <div className="w-full h-1/2 flex flex-col gap-1">
             <span className="w-full text-lg text-center font-bold">{name}</span>
             <div className="w-full h-full flex flex-col gap-2 justify-center items-center">
