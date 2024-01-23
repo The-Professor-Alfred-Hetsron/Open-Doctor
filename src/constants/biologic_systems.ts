62f4121bd6eb9a82ff90db73e0d06f099b645665
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2024-01-23 15:55:45 +0100

    Added System Images

--- src/constants/biologic_systems.ts
+++ src/constants/biologic_systems.ts
@@ -12,6 +12,8 @@ import {
   UrinaryCases
 } from "@/data/caseIndexer";
 
+const baseFolder = "/assets/img/systems"
+
 // Exemple de liste de systèmes biologiques
 export const BIOLOGICAL_SYSTEMS: IBioSystem[] = [
   {
@@ -19,69 +21,69 @@ export const BIOLOGICAL_SYSTEMS: IBioSystem[] = [
     name: "Système Cardiovasculaire",
     cases: CardiovascularCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/cardiovascular.jpg`,
   },
   {
     id: 2,
     name: "Système Respiratoire",
     cases: RespiratoryCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/respiratory.jpeg`,
   },
   {
     id: 3,
     name: "Système Digestif",
     cases: DigestiveCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/digestive.jpeg`,
   },
   {
     id: 4,
     name: "Système Nerveux",
     cases: NervousCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/nervous.jpeg`,
   },
   {
     id: 5,
     name: "Système Endocrinien",
     cases: EndocrineCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/endocrine.jpeg`,
   },
   {
     id: 6,
     name: "Système Musculaire",
     cases: MuscularCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/muscular.jpeg`,
   },
   {
     id: 7,
     name: "Système Squelettique",
     cases: SkeletalCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/skeletal.jpg`,
   },
   {
     id: 8,
     name: "Système Immunitaire",
     cases: ImmuneCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/immune.jpeg`,
   },
   {
     id: 9,
     name: "Système Urinaire",
     cases: UrinaryCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/urinary.png`,
   },
   {
     id: 10,
     name: "Système Reproducteur",
     cases: ReproductiveCases.length,
     difficulty: "MOYEN",
-    image: "/assets/img/logo.png",
+    image: `${baseFolder}/reproductive.jpeg`,
   },
 ];
